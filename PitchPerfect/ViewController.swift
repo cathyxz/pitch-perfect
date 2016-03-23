3884efc82fb6c3185ab0ff7d71e4992b52ced88f
Cathy Zhu <cathyxz@gmail.com>
2016-03-23 12:41:09 -0700

    Added button and change label on click.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,10 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordingLabel.text = "Recording in progress"
+    }
+
+    
 }
 
