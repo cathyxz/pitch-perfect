b7ed0c6d72724c7f9b575c95765e626741439c16
Cathy Zhu <cathyxz@gmail.com>
2016-03-24 12:48:10 -0700

    added new view controller and segue

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var stopRecordingButton: UIButton!
     
     @IBOutlet weak var recordingLabel: UILabel!
@@ -22,14 +22,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+        
+    }
+    
     @IBAction func recordAudio(sender: AnyObject) {
         recordingLabel.text = "Recording in progress"
+        recordButton.enabled = false
+        stopRecordingButton.enabled = true
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        stopRecordingButton.enabled = false
+        recordButton.enabled = true
+        recordingLabel.text = "Tap to record"
     }
 
     
